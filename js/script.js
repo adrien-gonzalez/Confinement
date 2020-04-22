5c453f468a3cc73c91a3433f7a6554ac8cf87ceb
adrien <adrien.gonzalez@laplateforme.io>
2020-04-22 14:50:57 +0200

    Inscription

---
+++ js/script.js
@@ -0,0 +1,86 @@
+// INSCRIPTION
+
+function ajax(){
+
+	$.ajax({
+			type:"POST",
+			url: url,
+			data : data,
+
+
+			success:function(data)
+			{
+				switch (data) {
+				case "Les caractères spéciaux ne sont pas acceptés":
+
+					document.getElementById("login").placeholder = '*Les caractères spéciaux ne sont pas acceptés'
+					break
+
+				case "utilisateur existant":
+
+					$("#login").val("");
+					document.getElementById("login").placeholder = '*Un compte avec ce login existe déjà'
+					$('#login').css({"border":"1px solid #C0392B"})
+					document.getElementById("login").className="erreur"	
+					break
+
+				case "Login trop court":
+
+					$("#login").val("");
+					document.getElementById("login").placeholder = '*Login trop court'
+					$('#login').css({"border":"1px solid #C0392B"})
+					document.getElementById("login").className="erreur"
+					break
+
+				case "Cet email est déjà utilisé":
+
+					$("#email").val("");
+					document.getElementById("email").placeholder = '*Cet email est déjà utilisé'
+					$('#email').css({"border":"1px solid #C0392B"})
+					document.getElementById("email").className="erreur"
+					break
+
+				case "Mot de passe trop court":
+
+					$("#password").val("");
+					document.getElementById("password").placeholder = '*Mot de passe trop court'
+					$('#password').css({"border":"1px solid #C0392B"})
+					document.getElementById("password").className="erreur"
+					break
+
+				case "Mots de passe différents":
+
+					$("#password2").val("");
+					document.getElementById("password2").placeholder = '*Les mots de passe ne correspondent pas'
+					$('#password2').css({"border":"1px solid #C0392B"})
+					document.getElementById("password2").className="erreur"
+					break
+
+				case "Inscrit":
+
+					// REDIRECTION
+					break
+				}
+			}
+		});
+}
+
+// CLIQUE SUR LE BOUTON INSCRIPTION
+$(document).ready(function(){
+	$("body").on("click","#register",function(){
+
+		if($("#nom").val() !="" && $("#prenom").val() !="" && $("#login").val() !="" && $("#email").val() !="" && $("#password").val() !="" && $("#password2").val() !="")
+		{
+			nom        = $("#nom").val();
+			prenom     = $("#prenom").val();
+			login  	   = $("#login").val();
+			email      = $("#email").val();
+			password   = $("#password").val();
+			password2  = $("#password2").val();
+				
+			url="../functions/function_register.php"
+			data={nom: nom, prenom: prenom, login: login, email: email, password: password, password2: password2}
+			ajax()
+		}
+	});
+});
---
+++ sources/inscription.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title>Accueil</title>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=0.7">
+		<!-- <link href="sources/style.css" rel="stylesheet"> -->
+		<!-- JQUERY -->
+		<script type="text/javascript" src="../js/jquery-3.5.0.min"></script>
+		<!-- MON SCRIPT -->
+		<script type="text/javascript" src="../js/script.js"></script>
+	</head>
+
+<body class="inscription">
+	<main>
+		<div>
+	  		<div>
+			    <!-- erreur inscription-->
+			    <p id="erreur_log"></p>
+			    <!-- Login Form -->
+			    <form>
+			    	<input type="text" id="login" name="login" placeholder="Login (5 caractères minimum)">
+			    	<input type="text" id="nom" name="nom" placeholder="Nom">
+			    	<input type="text" id="prenom" name="prenom" placeholder="Prenom">
+			        <input type="email" id="email" name="email" placeholder="Email">
+			        <input type="password" id="password"  name="password" placeholder="Password (8 caractères minimum)">
+			        <input type="password" id="password2"  name="password2" placeholder="Confirm password">
+			        <input type="button" id="register" value="Créer un compte">
+			    </form>
+	  		</div>
+		</div>
+	</main>
+</body>
