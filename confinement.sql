7070ed8b3f7ce0ab3896571647207221018341f6
adrien-gonzalez <adrien.gonzalez@laplateforme.io>
2020-04-24 14:53:07 +0200

    BDD

--- confinement.sql
+++ confinement.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 23 avr. 2020 à 16:39
--- Version du serveur :  10.4.10-MariaDB
--- Version de PHP :  7.3.12
+-- Généré le :  ven. 24 avr. 2020 à 12:52
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -46,6 +46,28 @@ CREATE TABLE IF NOT EXISTS `annonces` (
 INSERT DELAYED INTO `annonces` (`id`, `id_utilisateur`, `type_aide`, `description`) VALUES
 (3, 7, 'course', 'Disponible pour faire vos courses'),
 (10, 7, 'conversation', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(50) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `messages`
+--
+
+INSERT DELAYED INTO `messages` (`id`, `author`, `content`, `created_at`) VALUES
+(1, 'Firefou', 'Salut', '2020-04-24 14:51:36');
 
 -- --------------------------------------------------------
 
